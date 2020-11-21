0b7bdaba895cbeb27d69bcea890453ee9f0c7132
Артём <artemti0@gmail.com>
2020-11-21 20:22:48 +0200

    fix: Deprecated order query

--- app/models/medical_procedure.rb
+++ app/models/medical_procedure.rb
@@ -1,5 +1,5 @@
 class MedicalProcedure < ApplicationRecord
 
   scope :filtered, ->(q) { where("name ILIKE ?", "%#{q}%") }
-  scope :sorted, ->(q) { order("name ILIKE '#{sanitize_sql_like(q)}%' desc, name") }
+  scope :sorted, ->(q) { order(Arel.sql("name ILIKE '#{sanitize_sql_like(q)}%' desc, name")) }
 end
