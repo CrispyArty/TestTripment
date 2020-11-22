05e54ada59c6684a957e2a7046f45e7d8460df45
Артём <artemti0@gmail.com>
2020-11-22 18:09:09 +0200

    chore: Corrected mistakes with rubocop

--- spec/services/parse_procedures_spec.rb
+++ spec/services/parse_procedures_spec.rb
@@ -7,20 +7,22 @@ RSpec.describe ParseProcedures do
 
   describe 'parse procedures from Wiki' do
     before do
-      obj = double("test html")
+      obj = double('test html')
 
-      allow(obj).to receive(:open).and_return File.new("#{Rails.root}/spec/fixtures/wiki_medical_procedures.html")
+      allow(obj).to receive(:open).and_return(
+        File.new("#{Rails.root}/spec/fixtures/wiki_medical_procedures.html")
+      )
 
       allow(URI).to receive(:parse).and_return obj
     end
     it 'fill table' do
-      expect(ParseProcedures.new.call).to be_eql("Parsed 94 new records")
+      expect(ParseProcedures.new.call).to be_eql('Parsed 94 new records')
     end
 
     it 'insert only new records' do
       FactoryBot.create :medical_procedure, name: 'Angiography'
       FactoryBot.create :medical_procedure, name: 'Ultrasonography'
-      expect(ParseProcedures.new.call).to be_eql("Parsed 92 new records")
+      expect(ParseProcedures.new.call).to be_eql('Parsed 92 new records')
     end
   end
 end
