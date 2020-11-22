1d91ef2fc4b8ae90be2ed32ba277e74a9e66be29
Артём <artemti0@gmail.com>
2020-11-22 18:07:16 +0200

    feat: Created test for wiki procedures parser

---
+++ spec/services/parse_procedures_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ParseProcedures do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'parse procedures from Wiki' do
+    before do
+      obj = double("test html")
+
+      allow(obj).to receive(:open).and_return File.new("#{Rails.root}/spec/fixtures/wiki_medical_procedures.html")
+
+      allow(URI).to receive(:parse).and_return obj
+    end
+    it 'fill table' do
+      expect(ParseProcedures.new.call).to be_eql("Parsed 94 new records")
+    end
+
+    it 'insert only new records' do
+      FactoryBot.create :medical_procedure, name: 'Angiography'
+      FactoryBot.create :medical_procedure, name: 'Ultrasonography'
+      expect(ParseProcedures.new.call).to be_eql("Parsed 92 new records")
+    end
+  end
+end
